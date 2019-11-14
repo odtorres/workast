1791629ddaf6ad5189976737bfcc09d5ff59f830
Oscar <odtorres891118@gmail.com>
2019-11-14 01:49:26 -0600

    refactor

--- src/security.policies.js
+++ src/security.policies.js
@@ -2,7 +2,6 @@ const config = require("./config")
 
 exports.applyToken = app => {
     app.use((req, res, next) => {
-        console.log(config.API_TOKEN + " === " + req.query.token)
         if (config.API_TOKEN === req.query.token) {
             return next()
         }
