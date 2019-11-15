b9b665c63d7f7148d3d90203735c6c24e262f477
Oscar <odtorres891118@gmail.com>
2019-11-14 18:12:00 -0600

    Refactor user model

--- src/modules/users/user.model.js
+++ src/modules/users/user.model.js
@@ -1,12 +1,12 @@
-module.exports = function User(name, avatar) {
+exports.User = function User({ name, avatar }) {
   if (!name) {
     throw new Error('User must have a name.')
   }
   if (!avatar) {
     throw new Error("User must have an avatar.")
   }
-  return Object.freeze({
+  return {
     name: name,
     avatar: avatar
-  });
+  };
 }
