1d4187c9c86bb44f144734273146921c89520875
Oscar <odtorres891118@gmail.com>
2019-11-14 18:11:09 -0600

    Refactor status code

--- src/modules/users/user.controller.js
+++ src/modules/users/user.controller.js
@@ -1,34 +1,35 @@
+const config = require("../../config")
 const UserService = require('./user.service')
 
 exports.insert = async function (req, res) {
     try {
         const user = await UserService.insert(req.body);
-        return res.status(200).json({ "success": true, name: user.name });
+        return res.status(config.HTTP_STATUS_CODE["OK"]).json({ "success": true, name: user.name });
     } catch (e) {
-        return res.status(500).json(e);
+        return res.status(config.HTTP_STATUS_CODE["Bad Request"]).json(e.message);
     }
 }
 
 exports.remove = async function (req, res) {
     try {
         const count = await UserService.remove(req.params.id);
-        return res.status(200).json({ "success": true, "count": count });
+        return res.status(config.HTTP_STATUS_CODE["OK"]).json({ "success": true, "count": count });
     } catch (e) {
-        return res.status(500).json(e);
+        return res.status(config.HTTP_STATUS_CODE["Bad Request"]).json(e.message);
     }
 }
 
 exports.update = async function (req, res) {
     try {
         const object = await UserService.update(req.params.id, req.body);
-        return res.status(200).json({ "success": true, "update": object
+        return res.status(config.HTTP_STATUS_CODE["OK"]).json({ "success": true, "update": object
      });
     } catch (e) {
-        return res.status(500).json(e);
+        return res.status(config.HTTP_STATUS_CODE["Bad Request"]).json(e.message);
     }
 }
 
 exports.findAll = async function (req, res) {
     let users = await UserService.findAll()
-    return res.json(users)
+    return res.status(config.HTTP_STATUS_CODE["OK"]).json(users)
 }
